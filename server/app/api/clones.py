5a5d535d5492deaf8c503b7dad329fcf8b6c3717
Jonny Kernes <jmkernes@ucla.edu>
2023-05-20 18:53:16 -0500

    Dependency injection. Remove duplicated Clone class. Wrap CRUD ops in a transaction

--- server/app/api/clones.py
+++ server/app/api/clones.py
@@ -1,7 +1,7 @@
+from typing import Annotated
 from app.db import get_async_session
-from app.models import Clone, User
-from app.schemas import Clone, CloneCreate, User, UserCreate
-from fastapi import HTTPException
+from app import models, schemas
+from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -12,31 +12,35 @@ router = APIRouter(
 )
 
 
-@router.get("/{clone_id}", response_model=Clone)
-async def get_clone(clone_id: str):
-    async with get_async_session() as session:
-        clone = await session.get(Clone, clone_id)
-        if not clone:
-            raise HTTPException(status_code=404, detail="Clone not found")
-        return clone
+@router.get("/{clone_id}", response_model=schemas.Clone)
+async def get_clone(
+    clone_id: str, db: Annotated[AsyncSession, Depends(get_async_session)]
+):
+    clone = await db.get(models.Clone, clone_id)
+    if not clone:
+        raise HTTPException(status_code=404, detail="Clone not found")
+    return clone
 
 
-@router.post("/", response_model=Clone, status_code=201)
-async def create_clone(clone_create: CloneCreate):
-    async with get_async_session() as session:
-        clone = Clone(**clone_create.dict())
-        session.add(clone)
-        await session.commit()
-        await session.refresh(clone)
+@router.post("/", response_model=schemas.Clone, status_code=201)
+async def create_clone(
+    clone_create: schemas.CloneCreate,
+    db: Annotated[AsyncSession, Depends(get_async_session)],
+):
+    async with db.begin():
+        clone = models.Clone(**clone_create.dict())
+        db.add(clone)
+        await db.refresh(clone)
         return clone
 
 
 @router.delete("/{clone_id}")
-async def delete_clone(clone_id: str):
-    async with get_async_session() as session:
-        clone = await session.get(Clone, clone_id)
+async def delete_clone(
+    clone_id: str, db: Annotated[AsyncSession, Depends(get_async_session)]
+):
+    async with db.begin():
+        clone = await db.get(models.Clone, clone_id)
         if not clone:
             raise HTTPException(status_code=404, detail="Clone not found")
-        session.delete(clone)
-        await session.commit()
+        db.delete(clone)
         return {"message": "Clone deleted"}
--- server/app/api/users.py
+++ server/app/api/users.py
@@ -1,9 +1,8 @@
 from typing import Annotated, List
 
 from app.db import get_async_session
-from app.models import Clone, User
-from app.schemas import Clone, CloneCreate, User, UserCreate
-from fastapi import HTTPException
+from app import models, schemas
+from fastapi import HTTPException, Depends
 from fastapi.routing import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,42 +13,49 @@ router = APIRouter(
 )
 
 
-@router.get("/{user_id}", response_model=User)
-async def get_user(user_id: str):
-    async with get_async_session() as session:
-        user = await session.get(User, user_id)
+@router.get("/{user_id}", response_model=schemas.User)
+async def get_user(
+    user_id: str, db: Annotated[AsyncSession, Depends(get_async_session)]
+):
+    user = await db.get(models.User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.post("/", response_model=schemas.User, status_code=201)
+async def create_user(
+    user_create: schemas.UserCreate,
+    db: Annotated[AsyncSession, Depends(get_async_session)],
+):
+    async with db.begin():
+        new_user = models.User(email=user_create.email)
+        db.add(new_user)
+        await db.refresh(new_user)
+    return new_user
+
+
+@router.get("/{user_id}/clones", response_model=List[schemas.Clone])
+async def get_user_clones(
+    user_id: str, db: Annotated[AsyncSession, Depends(get_async_session)]
+):
+    user = await db.get(models.User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.clones
+
+
+@router.post("/{user_id}/clones", response_model=schemas.Clone, status_code=201)
+async def create_user_clone(
+    user_id: str,
+    clone_create: schemas.CloneCreate,
+    db: Annotated[AsyncSession, Depends(get_async_session)],
+):
+    async with db.begin():
+        user = await db.get(models.User, user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
-        return user
-
-
-@router.post("/", response_model=User, status_code=201)
-async def create_user(user_create: UserCreate):
-    async with get_async_session() as session:
-        new_user = User(email=user_create.email)
-        session.add(new_user)
-        await session.commit()
-        await session.refresh(new_user)
-        return new_user
-
-
-@router.get("/{user_id}/clones", response_model=List[Clone])
-async def get_user_clones(user_id: str):
-    async with get_async_session() as session:
-        user = await session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        return user.clones
-
-
-@router.post("/{user_id}/clones", response_model=Clone, status_code=201)
-async def create_user_clone(user_id: str, clone_create: CloneCreate):
-    async with get_async_session() as session:
-        user = await session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        new_clone = Clone(**clone_create.dict(), user_id=user_id)
-        session.add(new_clone)
-        await session.commit()
-        await session.refresh(new_clone)
-        return new_clone
+        new_clone = models.Clone(**clone_create.dict(), user_id=user_id)
+        db.add(new_clone)
+        await db.refresh(new_clone)
+    return new_clone
