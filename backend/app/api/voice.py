a056524466553a831b89caca0793af1b8af3c497
jonny kernes <jmkernes@g.ucla.edu>
2023-05-28 03:31:06 -0500

    basic file upload

--- backend/app/api/voice.py
+++ backend/app/api/voice.py
@@ -26,11 +26,7 @@ async def create_upload_files(files: list[UploadFile]):
 async def main():
     content = """
 <body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<form action="/voice/uploadfiles" enctype="multipart/form-data" method="post">
 <input name="files" type="file" multiple>
 <input type="submit">
 </form>
