8869f4a35a6b39eec38d23fcb43ae11a3671328f
Kevin Huo <kevin.w.huo@gmail.com>
2023-06-24 11:50:03 -0400

    add prompt templates for memory and reflections

--- backend/clonr/templates.py
+++ backend/clonr/templates.py
@@ -8,7 +8,8 @@ env = jinja2.Environment(
 
 
 class BasicChat:
-    template = env.from_string("""\
+    template = env.from_string(
+        """\
 {{ llm.system_start -}}
 {% if (system_prompt) -%}
 {{ system_prompt -}}
@@ -22,17 +23,14 @@ class BasicChat:
 {{- llm.user_end }}
 
 {{ llm.assistant_start -}}\
-""")
+"""
+    )
 
     @classmethod
-    def render(
-        cls, 
-        llm: LLM, 
-        user_prompt: str, 
-        system_prompt: str | None = None
-    ):
-        return cls.template.render(llm=llm, user_prompt=user_prompt, system_prompt=system_prompt)
-
+    def render(cls, llm: LLM, user_prompt: str, system_prompt: str | None = None):
+        return cls.template.render(
+            llm=llm, user_prompt=user_prompt, system_prompt=system_prompt
+        )
 
 
 class SummaryExample(BaseModel):
@@ -45,7 +43,8 @@ class SummaryExampleWithContext(SummaryExample):
 
 
 class Summarize:
-    template = env.from_string("""\
+    template = env.from_string(
+        """\
 {#### SYSTEM PROMPT ####}\
 {{ llm.system_start -}}
 {{ system_prompt }}
@@ -67,12 +66,13 @@ fuck
 {{- llm.user_end }}
 
 {{ llm.assistant_start -}}\
-""")
+"""
+    )
 
     @classmethod
     def render(
-        cls, 
-        llm: LLM, 
+        cls,
+        llm: LLM,
         passage: str,
         system_prompt: str | None = None,
         examples: list[SummaryExample] | None = None,
@@ -80,14 +80,15 @@ fuck
         if system_prompt is None:
             system_prompt = llm.default_system_prompt
         return cls.template.render(
-            llm=llm, 
+            llm=llm,
             system_prompt=system_prompt,
             examples=examples or [],
         )
 
 
 class SummarizeWithContext:
-    template = env.from_string("""\
+    template = env.from_string(
+        """\
 {#### SYSTEM PROMPT ####}\
 {{ llm.system_start -}}
 {{ system_prompt }}
@@ -119,13 +120,14 @@ Summary: {{ example.summary }}
 Context: {{ context }}
 Passage: {{ passage }}
 Summary:\
-""")
+"""
+    )
 
     @classmethod
     def render(
-        cls, 
-        llm: LLM, 
-        context: str, 
+        cls,
+        llm: LLM,
+        context: str,
         passage: str,
         system_prompt: str | None = None,
         examples: list[dict] | None = None,
@@ -135,17 +137,136 @@ Summary:\
         if examples is None:
             examples = []
         return cls.template.render(
-            llm=llm, 
-            context=context, 
+            llm=llm,
+            context=context,
             passage=passage,
             system_prompt=system_prompt,
             examples=examples,
         )
-    
+
     # @classmethod
     # def dummy_render(cls, *args)
 
-# class Instruct:
+
+# Given an observation, rate the significance of it
+class Memory:
+    template = env.from_string(
+        """\
+{#### SYSTEM PROMPT ####}\
+{{ llm.system_start -}}
+{{ system_prompt }}
+{{- llm.system_end }}
+
+{#### USER START ####}\
+{{ llm.user_start -}}\
+
+{#### EXPLAIN THE MEMORY TASK ####}\
+Given the following memory, rate the significance of the memory. \
+Use a scale of 1 to 10, where 1 is purely mundane (e.g., brushing teeth, making bed) and 10 is extremely poignant (e.g., a break up, college acceptance). \
+Rate the likely poignancy of the following piece of memory. \
+Respond with a single integer.
+\
+{#### MEMORY ####}
+Memory: {{ memory }}
+\
+{{- llm.user_end }}
+
+{{ llm.assistant_start -}}\
+"""
+    )
+
+    @classmethod
+    def render(
+        cls,
+        llm: LLM,
+        memory: str,
+        system_prompt: str | None = None,
+    ):
+        if system_prompt is None:
+            system_prompt = llm.default_system_prompt
+        return cls.template.render(llm=llm, system_prompt=system_prompt, memory=memory)
+
+
+# Retrieve most salient questions for reflection
+class ReflectionRetrieval:
+    template = env.from_string(
+        """\
+{#### SYSTEM PROMPT ####}\
+{{ llm.system_start -}}
+{{ system_prompt }}
+{{- llm.system_end }}
+
+{#### USER START ####}\
+{{ llm.user_start -}}\
+
+{#### RECENT MEMORIES ####}
+Recent memories: {{ recent_memories }} 
+\
+
+{#### EXPLAIN THE REFLECTION RETRIEVAL TASK ####}\
+Given only the information above on recent memories, what are 3 most salient high-level questions we can answer about the subjects in the statements? \
+\
+
+{{- llm.user_end }}
+
+{{ llm.assistant_start -}}\
+"""
+    )
+
+    @classmethod
+    def render(
+        cls,
+        llm: LLM,
+        memories: str,
+        system_prompt: str | None = None,
+    ):
+        if system_prompt is None:
+            system_prompt = llm.default_system_prompt
+        return cls.template.render(
+            llm=llm, system_prompt=system_prompt, memories=memories
+        )
+
+
+# Generate actual reflections / insights based on retrieved salient memories
+class ReflectionGeneration:
+    template = env.from_string(
+        """\
+{#### SYSTEM PROMPT ####}\
+{{ llm.system_start -}}
+{{ system_prompt }}
+{{- llm.system_end }}
+
+{#### USER START ####}\
+{{ llm.user_start -}}\
+
+{#### RELEVANT MEMORIES ####}
+Relevant memories: {{ relevant_memories }}
+\
+
+{#### EXPLAIN THE REFLECTION GENERATION TASK ####}\
+What 5 high-level insights can you infer from the above statements of relevant memories? \
+\
+
+{{- llm.user_end }}
+
+{{ llm.assistant_start -}}\
+"""
+    )
+
+    @classmethod
+    def render(
+        cls,
+        llm: LLM,
+        relevant_memories: str,
+        system_prompt: str | None = None,
+    ):
+        if system_prompt is None:
+            system_prompt = llm.default_system_prompt
+        return cls.template.render(
+            llm=llm, system_prompt=system_prompt, relevant_memories=relevant_memories
+        )
+
+    # class Instruct:
     """\
 Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
