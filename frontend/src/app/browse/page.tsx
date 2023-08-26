07c568140dd45bcf1dd94b71e4e22dcaf27d3182
Holland Pleskac <hollandpleskac@gmail.com>
2023-08-26 12:48:05 -0700

    browse page state update

--- frontend/src/app/browse/page.tsx
+++ frontend/src/app/browse/page.tsx
@@ -1,42 +1,14 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import BrowsePage from '@/components/BrowsePage'
-import PlusModal from '@/components/PlusModal'
-import { carouselData } from '@/data/carouselData'
 import Footer from '@/components/Footer'
-import AuthModal from '@/components/AuthModal'
-import { useQueryClones } from '@/hooks/useClones'
-
-async function fetchCharacters() {
-  return carouselData
-}
 
 export default function Browse() {
-  const [characters, setCharacters] = useState([])
-
-  const queryParams = {
-    sort: 'top',
-    offset: 0,
-    limit: 20
-  }
-  const {data, error, isLoading} = useQueryClones(queryParams);
-
-  useEffect(() => {
-    if(!isLoading && data) {
-      setCharacters(data)
-    }
-  }, [data])
-
-  if (isLoading || !characters || characters.length === 0) {
-    return <div>Loading...</div>
-  }
 
   return (
     <>
       <main className='w-full flex flex-col h-full'>
         <BrowsePage
-          initialCharacters={characters}
         />
         
       </main>
--- frontend/src/components/BrowsePage/index.tsx
+++ frontend/src/components/BrowsePage/index.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
-
-const axios = require('axios').default
 
 import TopBarStatic from '@/components/TopBarStatic'
 import AlertBar from '@/components/AlertBar'
@@ -15,98 +13,40 @@ import { Character, Tag } from '@/types'
 import AuthModal from '../AuthModal'
 import SearchGrid from '@/components/HomePage/SearchGrid'
 import TagComponent from './Tag'
-import SearchIcon from '../SearchIcon'
-import XIcon from '../XIcon'
 import Dropdown from './Dropdown'
 import { useQueryTags } from '@/hooks/useTags'
 import { useQueryClones } from '@/hooks/useClones'
 import { CloneSortType } from '@/client/models/CloneSortType'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Scrollbar } from 'swiper/modules'
+import { ColorRing } from 'react-loader-spinner'
 
-interface BrowsePageProps {
-    initialCharacters: Character[]
-}
-
-export default function BrowsePage({
-    initialCharacters
-}: BrowsePageProps) {
-    const [trill, setTrill] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const duration = 500
-
-    // search input state
-    const [searchInput, setSearchInput] = useState('')
-    const [isInputActive, setInputActive] = useState(false)
-    const handleInputFocus = () => setInputActive(true)
-    const handleInputBlur = () => {
-        if (searchInput === '') setInputActive(false)
-    }
-    const inputRef = useRef<HTMLInputElement>(null)
-    const didMountRef = useRef(false);
-
+export default function BrowsePage() {
     // tags state
-    const [tags, setTags] = useState<Tag[]>([])
+    const { data: tags, error: tagsError, isLoading: isLoadingTags } = useQueryTags();
     const [activeTag, setActiveTag] = useState<Tag | null>()
 
     // Sort state
     const [activeSort, setActiveSort] = useState<string>("Trending")
     const [activeSortType, setActiveSortType] = useState<string>("TOP")
 
-    // search grid characters state
-    const [searchedCharacters, setSearchedCharacters] = useState<any[]>(initialCharacters)
-    const [doneSearching, setDoneSearching] = useState(true)
+    // infinite scroll state
     const [hasMoreData, setHasMoreData] = useState(true)
-
-    const { data: tagsData, error: tagsError, isLoading: isLoadingTags } = useQueryTags();
-
-    useEffect(() => {
-        if (!isLoadingTags && tagsData) {
-            setTags(tagsData)
-            setDoneSearching(true)
-            setLoading(false)
-        }
-    }, [tagsData, isLoadingTags])
 
     const fetchMoreGridData = () => {
         // TODO: edit, incorporate useSWRInfinite on infinite scroll side
     }
 
-
-    useEffect(() => {
-        require('preline')
-    }, [])
-
+    // search grid characters state
     const searchQueryParams = {
         tags: activeTag ? [activeTag.id] : null,
-        name: searchInput,
         sort: CloneSortType[activeSortType],
-        similar: searchInput,
         offset: 0,
         limit: 20
     }
-    const { data: searchData, isLoading: isLoadingSearch } = useQueryClones(searchQueryParams);
+    const { data: characters, isLoading: isLoadingCharacters } = useQueryClones(searchQueryParams);
 
-    useEffect(() => {
-        if (!isLoadingSearch && searchData) {
-            console.log("this is activeTag: ", activeTag)
-            setSearchedCharacters(searchData)
-            setDoneSearching(true)
-            setLoading(false)
-            console.log("this is searchData: ", searchData)
-        }
-    }, [searchInput, isLoadingSearch, activeTag, activeSortType])
-
-
-
-    function handleTagClick(tag: Tag) {
-        console.log("clicked tag: ", tag)
-        setActiveTag(tag)
-        setSearchInput('')
-        // can update searched characters here
-    }
-
+    // handle sorting logic
     function mapSortClickToSortType(sort: string) {
         switch (sort) {
             case "Trending":
@@ -123,20 +63,14 @@ export default function BrowsePage({
     }
 
     function handleSortClick(sort: string) {
-        console.log("sort", sort)
         const sort_type = mapSortClickToSortType(sort)
-        console.log("sort_type: ", sort_type)
         setActiveSort(sort)
         setActiveSortType(sort_type)
     }
 
-    if (tags.length === 0) {
-        return (
-            <div> Loading tags... </div>
-        )
-    }
-
-
+    useEffect(() => {
+        require('preline')
+    }, [])
 
     return (
         <div className=''>
@@ -146,50 +80,74 @@ export default function BrowsePage({
             />
 
             <div className='flex px-[4%] gap-x-8 mt-[50px]'  >
-                <Swiper
-                    modules={[Navigation, Pagination, Scrollbar]}
-                    navigation={true}
-                    spaceBetween={4}
-                    slidesPerView={'auto'}
-                    slidesPerGroup={5}
-                    speed={1100}
-                    className={`w-full flex gap-x-2`}
-                    style={{
-                        zIndex: 50,
-                    }}
-                >
-                    {tags.map((tag, index) => {
-                        return (
-                            <SwiperSlide
-                                key={tag.id}
-                                className='w-auto inline-flex flex-grow flex-shrink-0'
-                                style={{
-                                    width: 'auto'
-                                }}
-                            >
-                                <TagComponent
-                                    name={tag.name}
-                                    onClick={() => {
-                                        handleTagClick(tag)
+                {isLoadingTags && (
+                    <div className='w-full flex-grow text-white' >&nbsp;</div>
+                )}
+                {!isLoadingTags && (
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar]}
+                        navigation={true}
+                        spaceBetween={4}
+                        slidesPerView={'auto'}
+                        slidesPerGroup={5}
+                        speed={1100}
+                        className={`w-full flex gap-x-2`}
+                        style={{
+                            zIndex: 50,
+                        }}
+                    >
+                        {tags!.map((tag, index) => {
+                            return (
+                                <SwiperSlide
+                                    key={tag.id}
+                                    className='w-auto inline-flex flex-grow flex-shrink-0'
+                                    style={{
+                                        width: 'auto'
                                     }}
-                                    active={tag.id === activeTag?.id}
+                                >
+                                    <TagComponent
+                                        name={tag.name}
+                                        onClick={() => {
+                                            setActiveTag(tag)
+                                        }}
+                                        active={tag.id === activeTag?.id}
 
-                                />
-                            </SwiperSlide>
-                        )
-                    })}
-                </Swiper>
+                                    />
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                )}
                 <Dropdown onItemClick={handleSortClick} activeSort={activeSort} />
             </div>
 
-            <SearchGrid
-                characters={searchedCharacters}
-                doneSearching={doneSearching}
-                fetchMoreData={fetchMoreGridData}
-                hasMoreData={hasMoreData}
-                showPadding2={true}
+            {isLoadingCharacters && (
+                <div className="grid place-items-center" 
+                style={{
+                    height: "calc(100vh - 50px - 75px - 80px)"
+                }}>
+                <ColorRing
+                  visible={true}
+                  height="80"
+                  width="80"
+                  ariaLabel="blocks-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="blocks-wrapper"
+                  colors={['#9333ea', '#9333ea', '#9333ea', '#9333ea', '#9333ea']}
+                />
+              </div>
+            )}
 
-            />
+            {!isLoadingCharacters && (
+                <SearchGrid
+                    characters={characters}
+                    doneSearching={!isLoadingCharacters}
+                    fetchMoreData={fetchMoreGridData}
+                    hasMoreData={hasMoreData}
+                    showPadding2={true}
+
+                />
+            )}
 
             <AuthModal />
         </div>
