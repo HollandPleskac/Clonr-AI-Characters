73ea347b2e6a6d1432862926754f66472bb4255c
Kevin Huo <kevin.w.huo@gmail.com>
2023-08-28 01:08:40 -0400

    add conversation continue endpoint

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -16,7 +16,7 @@ import { useQueryClones } from '@/hooks/useClones'
 import AuthModal from '../AuthModal'
 import { ColorRing } from "react-loader-spinner"
 import { CloneSortType } from '@/client/models/CloneSortType'
-import { useQueryConversationsSidebar } from '@/hooks/useConversations'
+import { useQueryConversationsContinue } from '@/hooks/useConversations'
 import { useClonesPagination } from '@/hooks/useClonesPagination'
 
 export default function HomeScreen() {
@@ -49,14 +49,7 @@ export default function HomeScreen() {
   // const { data: searchedCharacters, isLoading: isTopLoadingSearch } = useQueryClones(topSearchQueryParams);
   const { data: trendingChars, isLoading: isTrendingLoading } = useQueryClones(trendingQueryParams);
   const { data: topChars, isLoading: isTopLoading } = useQueryClones(topQueryParams);
-  const { data: continueConvos, isLoading: isLoadingContinue } = useQueryConversationsSidebar(conversationsSidebarParams)
-
-  // const continueChars = continueConvos?.map((convo) => {
-  //   return convo.clone
-  // })
-
-  // TODO: edit with new endpoint
-  const continueChars = [];
+  const { data: continueChars, isLoading: isLoadingContinue } = useQueryConversationsContinue(conversationsSidebarParams)
 
   useEffect(() => {
     if (searchInput === '') {
