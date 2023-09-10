53f33483b040339143e92f324c9e207ed042e21b
Kevin Huo <kevin.w.huo@gmail.com>
2023-09-10 01:03:23 -0400

    add posthog scaffold

--- frontend/src/auth/Providers.tsx
+++ frontend/src/auth/Providers.tsx
@@ -1,11 +1,36 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import posthog from 'posthog-js';
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 export default function NextAuthProvider ({ children }: Props) {
+  const posthogApiKey = process.env.NEXT_PUBLIC_POSTHOG_API_KEY;
+  posthog.init(posthogApiKey ? posthogApiKey : '', 
+    { api_host: 'https://app.posthog.com' }
+  )
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const pageIdentifier = `${pathname}${searchParams.toString()}`;
+    const devMode = process.env.NODE_ENV === 'development';
+
+    if (devMode) {
+      console.log("skipping posthog captures");
+    } else {
+      posthog.capture("$pageview", {
+        page_identifier: pageIdentifier,
+      });
+    }
+    
+  }, [pathname, searchParams]);
+
   return <SessionProvider>{children}</SessionProvider>;
 };
